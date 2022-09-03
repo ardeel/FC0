4a31a368d60c30d564d86dcc251062d4e13b59e1
AaronDeel <aarondeel@utexas.edu>
2022-09-02 19:30:00 -0500

    Upload

---
+++ README.txt
@@ -0,0 +1,18 @@
+1. Name: Aaron Deel
+2. EID: ARD3847
+3. Email: aarondeel@utexas.edu
+4. Time: 6 hours
+5. Slip days: 0
+6. Comments:  External resources found to help with layout:
+
+Set up constraint layout
+https://developer.android.com/develop/ui/views/layout/constraint-layout
+
+connect linear layout element to constraint layout
+https://stackoverflow.com/questions/60735942/place-elements-under-others-in-linear-layout-inside-constraint-layout
+
+Evently Space out elements in constraintview
+https://stackoverflow.com/questions/37518745/evenly-spacing-views-using-constraintlayout
+
+Set a dynamic (weight) width to linear layout objects
+https://stackoverflow.com/questions/6557220/defining-a-percentage-width-for-a-linearlayout
---
+++ app/src/main/java/edu/utap/fc0/MainActivity.kt
@@ -0,0 +1,33 @@
+package edu.utap.fc0
+
+import android.content.res.Configuration
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import edu.utap.fc0.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(resources.configuration.orientation  ==  Configuration.ORIENTATION_PORTRAIT) {
+
+        } else
+            if(resources.configuration.orientation ==  Configuration.ORIENTATION_LANDSCAPE) {
+
+            }
+    }
+}
